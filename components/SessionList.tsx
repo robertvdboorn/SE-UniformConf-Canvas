32081331aba49a22020dcef4b38cad1bd236e082
Robert van den Boorn <robertb@uniform.dev>
2025-08-27 13:26:32 +0200

    updated uniform packages and updated slot rendering

--- components/SessionList.tsx
+++ components/SessionList.tsx
@@ -1,33 +1,45 @@
-import { ComponentProps, registerUniformComponent, UniformSlot } from '@uniformdev/canvas-react';
-import React from 'react';
+import {
+  ComponentProps,
+  registerUniformComponent,
+  UniformSlot,
+  UniformText,
+} from "@uniformdev/canvas-react";
+import React from "react";
 
 export type SessionListProps = ComponentProps<{
   title: string;
 }>;
 
-function SessionList({ title }: SessionListProps) {
+function SessionsWrapper({ items }: { items: React.ReactNode[] }) {
+  return (
+    <>
+      {items.map((item, index) => (
+        <div
+          key={index}
+          className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink"
+        >
+          {item}
+        </div>
+      ))}
+    </>
+  );
+}
+
+function SessionList({ }: SessionListProps) {
   return (
     <fieldset>
       <section className="bg-white border-b py-24">
         <div className="container mx-auto flex flex-wrap pt-4 pb-12">
-          <h1
-            className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800"
-            dangerouslySetInnerHTML={{ __html: title }}
-          />
-          <UniformSlot name="sessions">
-            {({ child, key }) => (
-              <div key={key} className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-                {child}
-              </div>
-            )}
-          </UniformSlot>
+          <UniformText as="h1" className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800" parameterId="title" placeholder="Title goes here" />
+          <UniformSlot name="sessions" wrapperComponent={SessionsWrapper} />
         </div>
       </section>
     </fieldset>
   );
 }
+
 registerUniformComponent({
-  type: 'sessionList',
+  type: "sessionList",
   component: SessionList,
 });
 
